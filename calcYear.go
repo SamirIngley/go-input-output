e18b3ff661ee35631e43ca294ff7521df98696cc
SamirIngley <samir.ingle7@gmail.com>
2020-06-15 13:50:23 -0700

    input output calculate year done

--- calcYear.go
+++ calcYear.go
@@ -1,6 +1,29 @@
-import "time"
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "time"
+)
 
 // Create a program that calculates the year, given the date of birth and age
+
+
+// KEYBOARD IO
+func keyboard() {
+    reader := bufio.NewReader(os.Stdin)
+
+    fmt.Print("Enter age: ")
+    ioage, _ := reader.ReadString('\n')
+
+    fmt.Print("Enter dob: ")
+    iodob, _ := reader.ReadString('\n')
+
+    // fmt.Print(ioage, iodb)
+    return AgeYear(ioage, iodob)
+}
+
 
 // Calculates year based on age of person
 func AgeYear(dob time.Time, age time.Time) int{
